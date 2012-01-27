c6c2b77f2f00e8da0c22ca20563da05fad5cf76a
Dave Della Costa <dave-dellacosta@garage.co.jp>
2012-01-27 14:08:14 +0900

    Updates to match my configuration.

--- bin/titanium.sh
+++ bin/titanium.sh
@@ -6,9 +6,9 @@
 PROJECT_NAME=${PROJECT_NAME}
 PROJECT_ROOT=${PROJECT_ROOT:-../}
 APP_DEVICE=${DEVICE_TYPE}
-IPHONE_SDK_VERSION="4.2"
-TI_SDK_VERSION="1.6.1"
-TI_DIR="/Library/Application\ Support/Titanium"
+IPHONE_SDK_VERSION="5.0"
+TI_SDK_VERSION="1.8.0.1"
+TI_DIR="/Users/dave-dellacosta/Library/Application\ Support/Titanium"
 TI_ASSETS_DIR="${TI_DIR}/mobilesdk/osx/${TI_SDK_VERSION}"
 TI_IPHONE_DIR="${TI_ASSETS_DIR}/iphone"
 TI_BUILD="${TI_IPHONE_DIR}/builder.py"
